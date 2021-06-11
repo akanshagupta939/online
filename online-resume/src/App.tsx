96ec1f60ac6141b9d307c8e06853b4ba52134e3c
Akansha Gupta <59836921+agup0102@users.noreply.github.com>
2021-06-11 15:27:50 +1000

    interactive particle js

--- online-resume/src/App.tsx
+++ online-resume/src/App.tsx
@@ -21,63 +21,107 @@ const navigation = {
 };
 const particleConfig: any = {
   particles: {
-    color: {
-      value: "#014b3e",
-    },
     number: {
-      value: 160,
+      value: 80,
       density: {
-        enable: false,
+        enable: true,
+        value_area: 800,
+      },
+    },
+    color: {
+      value: ["#BD10E0", "#B8E986", "#50E3C2", "#FFD300", "#E86363"],
+    },
+    shape: {
+      type: "circle",
+      stroke: {
+        width: 0,
+        color: "#b6b2b2",
+      },
+    },
+    opacity: {
+      value: 0.5211089197812949,
+      random: false,
+      anim: {
+        enable: true,
+        speed: 1,
+        opacity_min: 0.1,
+        sync: false,
       },
     },
     size: {
-      value: 3,
+      value: 8.017060304327615,
       random: true,
       anim: {
-        speed: 4,
-        size_min: 0.3,
+        enable: true,
+        speed: 12.181158184520175,
+        size_min: 0.1,
+        sync: true,
       },
     },
     line_linked: {
-      color: {
-        value: "#032825",
-      },
       enable: true,
-      distance: 300,
+      distance: 150,
+      color: "#c8c8c8",
       opacity: 0.4,
-      width: 2,
+      width: 1,
     },
     move: {
-      random: true,
-      direction: "top",
-      out_mode: "out",
+      enable: true,
+      speed: 0.5,
+      direction: "none",
+      random: false,
+      straight: false,
+      out_mode: "bounce",
+      bounce: false,
+      attract: {
+        enable: false,
+        rotateX: 600,
+        rotateY: 1200,
+      },
     },
   },
   interactivity: {
+    detect_on: "canvas",
     events: {
       onhover: {
         enable: true,
         mode: "attract",
       },
       onclick: {
-        enable: true,
-        mode: "repulse",
+        enable: false,
+        mode: "push",
       },
+      resize: true,
     },
     modes: {
+      grab: {
+        distance: 400,
+        line_linked: {
+          opacity: 1,
+        },
+      },
       bubble: {
-        distance: 250,
+        distance: 400,
+        size: 40,
         duration: 2,
-        size: 0,
-        opacity: 0,
+        opacity: 8,
+        speed: 3,
       },
       repulse: {
-        distance: 400,
-        duration: 4,
+        distance: 200,
+        duration: 0.4,
+      },
+      push: {
+        particles_nb: 4,
+      },
+      remove: {
+        particles_nb: 2,
       },
     },
   },
+  retina_detect: true,
 };
+
 const resumeDate = {
   name: "Akansha",
   role: "Software Engineer",
